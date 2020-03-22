ccd26aea7a47fe7bb2b10af638eb33296ba2f406
Danilo Paggi <danilopaggi@gmail.com>
2020-03-22 13:49:28 -0300

    Create the error list

---
+++ src/app/Exceptions/apiExceptions.js
@@ -0,0 +1,21 @@
+module.exports = [
+  { id: 1, code: 400, message: "Action canceled! Invalid status" },
+  { id: 2, code: 404, message: "Action canceled! Exam not found" },
+  { id: 3, code: 404, message: "Action canceled! Exam name not found" },
+  { id: 4, code: 404, message: "Action canceled! Laboratory not found" },
+  {
+    id: 5,
+    code: 405,
+    message: "Action canceled! Invalid input at positions:"
+  },
+  {
+    id: 6,
+    code: 404,
+    message: "Action canceled! Exam not found at positions:"
+  },
+  {
+    id: 7,
+    code: 404,
+    message: "Action canceled! Laboratory not found at positions:"
+  }
+];
--- src/routes.js
+++ src/routes.js
@@ -2,21 +2,24 @@ const express = require("express");
 const routes = express.Router();
 const ctrlExam = require("./app/controllers/examController");
 const ctrlLaboratory = require("./app/controllers/laboratoryController");
+const mdlLab = require("./app/middlewares/laboratoryMiddleware");
+const mdlExam = require("./app/middlewares/examMiddleware");
 
-routes.get("/laboratory/index/:status", ctrlLaboratory.index);
-routes.get("/laboratory/show/:labId", ctrlLaboratory.show);
-routes.post("/laboratory/store", ctrlLaboratory.store);
-routes.put("/laboratory/update", ctrlLaboratory.update);
-routes.delete("/laboratory/destroy/:labsIds", ctrlLaboratory.destroy);
+routes.get("/laboratory/index/:status", mdlLab.index, ctrlLaboratory.index);
+routes.get("/laboratory/show/:labId", mdlLab.show, ctrlLaboratory.show);
+routes.post("/laboratory/store", mdlLab.store, ctrlLaboratory.store);
+routes.put("/laboratory/update", mdlLab.update, ctrlLaboratory.update);
+routes.delete("/laboratory/destroy/:labsIds", mdlLab.destroy, ctrlLaboratory.destroy);
 
-routes.get("/exam/:examName/laboratories", ctrlLaboratory.getByExamName);
+routes.get("/exam/:examName/laboratories", mdlLab.getByExamName, ctrlLaboratory.getByExamName);
 
-routes.get("/exam/index/:status", ctrlExam.index);
-routes.get("/exam/show/:examId", ctrlExam.show);
-routes.post("/exam/store", ctrlExam.store);
-routes.put("/exam/update", ctrlExam.update);
-routes.delete("/exam/destroy/:examsIds", ctrlExam.destroy);
+routes.get("/exam/index/:status", mdlExam.index, ctrlExam.index);
+routes.get("/exam/show/:examId", mdlExam.show, ctrlExam.show);
+routes.post("/exam/store", mdlExam.store, ctrlExam.store);
+routes.put("/exam/update", mdlExam.update, ctrlExam.update);
+routes.delete("/exam/destroy/:examsIds", mdlExam.destroy, ctrlExam.destroy);
 
-routes.post("/exam/:examId/linkLaboratory", ctrlExam.linkLaboratory);
-routes.delete("/exam/:examId/unlinkLaboratory", ctrlExam.unlinkLaboratory);
+routes.post("/exam/:examId/linkLaboratory", mdlExam.linkLaboratory, ctrlExam.linkLaboratory);
+routes.delete("/exam/:examId/unlinkLaboratory", mdlExam.unlinkLaboratory, ctrlExam.unlinkLaboratory);
+
 module.exports = routes;
