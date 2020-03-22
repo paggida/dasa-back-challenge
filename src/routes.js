c17861b06ce533347ce72917d3c99dca1fe495ab
Danilo Paggi <danilopaggi@gmail.com>
2020-03-21 23:16:22 -0300

    Swagger documentation feature

--- src/routes.js
+++ src/routes.js
@@ -3,19 +3,20 @@ const routes = express.Router();
 const ctrlExam = require("./app/controllers/examController");
 const ctrlLaboratory = require("./app/controllers/laboratoryController");
 
-routes.get("/exam/index", ctrlExam.index);
-routes.get("/exam/show/:examId", ctrlExam.show);
-routes.post("/exam/store", ctrlExam.store);
-routes.put("/exam/update", ctrlExam.update);
-routes.delete("/exam/destroy/:examsIds", ctrlExam.destroy);
-
-routes.get("/laboratory/index", ctrlLaboratory.index);
+//Adicionar a possibilidade de listar os ativos
+routes.get("/laboratory/index/:status", ctrlLaboratory.index);
 routes.get("/laboratory/show/:labId", ctrlLaboratory.show);
 routes.post("/laboratory/store", ctrlLaboratory.store);
 routes.put("/laboratory/update", ctrlLaboratory.update);
 routes.delete("/laboratory/destroy/:labsIds", ctrlLaboratory.destroy);
 
 routes.get("/exam/:examName/laboratories", ctrlLaboratory.getByExamName);
+
+routes.get("/exam/index/:status", ctrlExam.index);
+routes.get("/exam/show/:examId", ctrlExam.show);
+routes.post("/exam/store", ctrlExam.store);
+routes.put("/exam/update", ctrlExam.update);
+routes.delete("/exam/destroy/:examsIds", ctrlExam.destroy);
 
 routes.post("/exam/:examId/connectLaboratory", ctrlExam.connectLaboratory);
 routes.delete(
